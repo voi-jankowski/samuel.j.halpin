f0a1a94a247a63716ff3b2a8a85c126d9c40bae5
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2023-07-14 16:13:27 +1000

    Created index.js for models.

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+const User = require("./User");
+const Product = require("./Product");
+const Order = require("./Order");
+const Comment = require("./Comment");
+const Question = require("./Question");
+
+module.exports = { User, Product, Order, Comment, Question };
