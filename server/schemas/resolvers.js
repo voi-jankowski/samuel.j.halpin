ba7dda8b3846fff4af37dbec1f0328cd43b4efe0
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2023-07-16 16:07:08 +1000

    Created the first Mutation in resolvers: addUser.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -98,7 +98,14 @@ const resolvers = {
     },
   },
 
-  Mutation: {},
+  Mutation: {
+    // add a user
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
 };
 
 module.exports = resolvers;
