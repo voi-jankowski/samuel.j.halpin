eca8f0a95e7ba18a2bd48f29327bfe98cb2f89ea
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2023-07-17 21:55:41 +1000

    Constructed middleware to attach the JWT token to requests as header.

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,19 @@ const httpLink = createHttpLink({
   uri: "/graphql",
 });
 
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 import "./App.css";
 
 function App() {
