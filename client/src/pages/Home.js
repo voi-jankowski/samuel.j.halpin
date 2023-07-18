2bf6b7c38d38f674dfcd5414c190f1fe9970b9ce
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2023-07-18 20:05:13 +1000

    Created boxStyles and headingStyles as variables passed to the components.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,13 +2,28 @@ import { Box, Container, Heading, Text } from "@chakra-ui/react";
 import React from "react";
 
 function Home() {
+  const boxStyles = {
+    p: "10px",
+    border: "1px solid black",
+    borderRadius: "10px",
+    bg: "purple.500",
+    color: "white",
+    textAlign: "center",
+    ":hover": {
+      bg: "purple.800",
+      color: "white",
+    },
+  };
+  const headingStyles = {
+    my: "30px",
+    p: "10px",
+  };
+
   return (
-    <Container>
-      <Heading my="30px" p="10px">
-        About
-      </Heading>
-      <Box>
-        <Text marginLeft="30px">
+    <Container as="section" maxWidth="3xl" py="20px">
+      <Heading sx={headingStyles}>About</Heading>
+      <Box sx={boxStyles}>
+        <Text mx="20px">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
           euismod, nisl id aliquam ultricies, nunc nunc aliquet nunc, vitae
           aliquam nunc nunc vitae nunc. Donec euismod, nisl id aliquam
