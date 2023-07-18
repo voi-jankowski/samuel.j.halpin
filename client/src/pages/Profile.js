78d4cf23134fabb87ea8070e573764967fe207b5
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2023-07-19 09:39:34 +1000

    Inserted the template for Profile to be updatable for a user.

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,18 +2,120 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Login from "../components/Login";
 import ChangeColor from "../components/ChangeColor";
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  useColorModeValue,
+  HStack,
+  Avatar,
+  AvatarBadge,
+  IconButton,
+  Center,
+  Container,
+} from "@chakra-ui/react";
+import { SmallCloseIcon } from "@chakra-ui/icons";
 
 export default function Profile() {
   const user = useSelector((state) => state.user.value);
   const themeColor = useSelector((state) => state.theme.value);
   return (
-    <div style={{ color: themeColor }}>
-      <h1>Profile Page</h1>
-      <p>Username: {user.username}</p>
-      <p>Email: {user.email}</p>
-      <p>Password: {user.password}</p>
+    <Container style={{ color: themeColor }}>
+      <Flex
+        minH={"100vh"}
+        align={"center"}
+        justify={"center"}
+        bg={useColorModeValue("gray.50", "gray.800")}
+      >
+        <Stack
+          spacing={4}
+          w={"full"}
+          maxW={"md"}
+          bg={useColorModeValue("white", "gray.700")}
+          rounded={"xl"}
+          boxShadow={"lg"}
+          p={6}
+          my={12}
+        >
+          <Heading lineHeight={1.1} fontSize={{ base: "2xl", sm: "3xl" }}>
+            User Profile Edit
+          </Heading>
+          <FormControl id="userName">
+            <FormLabel>User Icon</FormLabel>
+            <Stack direction={["column", "row"]} spacing={6}>
+              <Center>
+                <Avatar size="xl" src="https://bit.ly/sage-adebayo">
+                  <AvatarBadge
+                    as={IconButton}
+                    size="sm"
+                    rounded="full"
+                    top="-10px"
+                    colorScheme="red"
+                    aria-label="remove Image"
+                    icon={<SmallCloseIcon />}
+                  />
+                </Avatar>
+              </Center>
+              <Center w="full">
+                <Button w="full">Change Icon</Button>
+              </Center>
+            </Stack>
+          </FormControl>
+          <FormControl id="userName" isRequired>
+            <FormLabel>User name</FormLabel>
+            <Input
+              placeholder={user.username}
+              _placeholder={{ color: "gray.500" }}
+              type="text"
+            />
+          </FormControl>
+          <FormControl id="email" isRequired>
+            <FormLabel>Email address</FormLabel>
+            <Input
+              placeholder={user.email}
+              _placeholder={{ color: "gray.500" }}
+              type="email"
+            />
+          </FormControl>
+          <FormControl id="password" isRequired>
+            <FormLabel>Password</FormLabel>
+            <Input
+              placeholder={user.password}
+              _placeholder={{ color: "gray.500" }}
+              type="password"
+            />
+          </FormControl>
+          <Stack spacing={6} direction={["column", "row"]}>
+            <Button
+              bg={"red.400"}
+              color={"white"}
+              w="full"
+              _hover={{
+                bg: "red.500",
+              }}
+            >
+              Cancel
+            </Button>
+            <Button
+              bg={"blue.400"}
+              color={"white"}
+              w="full"
+              _hover={{
+                bg: "blue.500",
+              }}
+            >
+              Submit
+            </Button>
+          </Stack>
+        </Stack>
+      </Flex>
+      <br />
       <Login />
       <ChangeColor />
-    </div>
+    </Container>
   );
 }
