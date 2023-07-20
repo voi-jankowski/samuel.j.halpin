25724e903d34147be58c0d2889c2049868e14746
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2023-07-20 16:17:18 +1000

    Added states and variables for signing up user to SignupCard component.

--- client/src/components/header/Signup.js
+++ client/src/components/header/Signup.js
@@ -21,9 +21,46 @@ import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import { useDispatch } from "react-redux";
 import { login, logout } from "../../features/user";
 
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../../utils/mutations";
+import { useHistory } from "react-router-dom";
+
+import Auth from "../../utils/auth";
+
 export default function SignupCard() {
   const dispatch = useDispatch();
   const [showPassword, setShowPassword] = useState(false);
+
+  const [formState, setFormState] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [addUser, { error, data }] = useMutation(ADD_USER);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+
+    try {
+      const { data } = await addUser({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <Flex
