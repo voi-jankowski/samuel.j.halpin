f0775f5ca53aadf5de9075c537e725082f5d9ad3
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2023-07-21 15:32:31 +1000

    Created the look for DeleteUser componenet in line with Signup and Login modal.

--- client/src/components/smallComponents/DeleteUser.js
+++ client/src/components/smallComponents/DeleteUser.js
@@ -24,5 +24,67 @@ import AuthService from "../../utils/auth";
 const Auth = new AuthService();
 
 export default function DeleteUser() {
-  return <div></div>;
+  const dispatch = useDispatch();
+  const [formState, setFormState] = useState({});
+
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"} color={"red.500"}>
+            Delete Account
+          </Heading>
+        </Stack>
+        <br />
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Stack spacing={4}>
+            <Text fontSize={"lg"} color={"red.400"}>
+              Are you sure you want to leave us?
+            </Text>
+            <Stack spacing={10}>
+              <Stack
+                direction={{ base: "column", sm: "row" }}
+                align={"start"}
+                justify={"space-between"}
+              >
+                <Checkbox>Tick the box to confirm</Checkbox>
+              </Stack>
+              <Stack spacing={6} direction={["column", "row"]}>
+                <Button
+                  bg={"blue.400"}
+                  color={"white"}
+                  w="full"
+                  _hover={{
+                    bg: "blue.500",
+                  }}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  bg={"red.400"}
+                  color={"white"}
+                  w="full"
+                  _hover={{
+                    bg: "red.500",
+                  }}
+                >
+                  Delete
+                </Button>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
 }
