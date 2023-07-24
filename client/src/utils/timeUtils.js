d2dcfd7ffe84d7840b4d3e1de986236cc54b0276
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2023-07-24 14:26:42 +1000

    Created timeUtils.js with methods to handle time difference and sorting selection of comments ot questions by createdAt.

---
+++ client/src/utils/timeUtils.js
@@ -0,0 +1,56 @@
+// methods sourced from https://github.com/blaqbox-prime/interactive_comments
+export const getTimeDifference = (date) => {
+  let today = new Date();
+  let givenDate = new Date(date);
+
+  let daysInBetween =
+    millisecondsToDays(today.getTime()) -
+    millisecondsToDays(givenDate.getTime());
+
+  if (daysInBetween <= 1) {
+    return "today";
+  }
+  if (daysInBetween > 7 && daysInBetween < 14) {
+    return "1 week ago";
+  }
+  if (daysInBetween > 14) {
+    return `${Math.floor(daysInBetween / 7)} weeks ago`;
+  }
+  if (daysInBetween > 29 && daysInBetween < 58) {
+    return `${Math.floor(daysInBetween / 4 / 7)} month ago`;
+  }
+  if (daysInBetween > 60) {
+    return `${Math.floor(daysInBetween / 4 / 7)} months ago`;
+  }
+  if (daysInBetween > 1) {
+    return `${daysInBetween} days ago`;
+  }
+};
+
+const millisecondsToDays = (miliseconds) => {
+  return Math.floor(miliseconds / 1000 / 60 / 60 / 24);
+};
+
+export function selectionSort(array) {
+  let temp;
+  let sortedArray = array;
+  let size = sortedArray.length;
+
+  for (let i = 0; i < size; i++) {
+    let min = sortedArray[i];
+    let min_index = i;
+
+    for (let j = i + 1; j < size; j++) {
+      if (sortedArray[j].score > min.score) {
+        min = sortedArray[j];
+        min_index = j;
+      }
+    }
+
+    temp = sortedArray[i];
+    sortedArray[i] = min;
+    sortedArray[min_index] = temp;
+  }
+
+  return sortedArray;
+}
