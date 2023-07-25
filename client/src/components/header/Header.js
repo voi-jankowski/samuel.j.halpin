d805b8f55178a2ea5c0093d8e57d162aeb54fdeb
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2023-07-25 17:55:59 +1000

    Created the logo in the Heading with Images and text and gave it reactive styling.

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -3,17 +3,38 @@ import {
   Box,
   Spacer,
   Heading,
+  Image,
   Flex,
+  Text,
   useColorModeValue,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 
 import Navbar from "./Navbar";
 
 function Header() {
+  const logoSize = useBreakpointValue({ base: "70%", md: "50%" });
+  const baloonSize = useBreakpointValue({ base: "30%", md: "20%" });
   return (
     <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
       <Flex as="header" alignItems="center">
-        <Heading as="h1">Samuel J. Halpin</Heading>
+        <Image
+          src="./assets/images/baloon.png"
+          alt="Heading Baloon"
+          width="20%"
+        />
+        <Flex direction="column" alignItems="right" width={logoSize}>
+          <Image src="./assets/images/sam-logo.png" alt="Sam's Logo" />
+          <Text
+            fontSize={{ base: "md", sm: "xl", md: "2xl", lg: "3xl", xl: "4xl" }}
+            fontWeight="bold"
+            color="gray.800"
+            alignSelf="flex-end"
+            mt={{ base: -1, sm: -2, md: -3, lg: -4, xl: -5 }}
+          >
+            Author
+          </Text>
+        </Flex>
         <Spacer />
       </Flex>
       <Navbar />
