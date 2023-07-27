1c0c75d7992e234001e4d4b4a95cca08e96cb603
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2023-07-27 13:36:11 +1000

    Added logic to Success page to save order and to clear the cart and redirect to homepage.

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,9 +1,35 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { ADD_ORDER } from "../utils/mutations";
+import { idbPromise } from "../utils/helpers";
 
 import { Box, Heading, Text, Flex, Spinner, Image } from "@chakra-ui/react";
 
 export default function Success() {
+  const [addOrder] = useMutation(ADD_ORDER);
+
+  useEffect(() => {
+    async function saveOrder() {
+      const cart = await idbPromise("cart", "get");
+      const products = cart.map((item) => item._id);
+
+      if (products.length) {
+        const { data } = await addOrder({ variables: { products } });
+        const productData = data.addOrder.products;
+
+        productData.forEach((item) => {
+          idbPromise("cart", "delete", item);
+        });
+      }
+
+      setTimeout(() => {
+        window.location.assign("/");
+      }, 3000);
+    }
+
+    saveOrder();
+  }, [addOrder]);
+
   return (
     <Box textAlign="center" py={10} px={6}>
       <Flex justifyContent="center" alignItems="center">
@@ -18,6 +44,9 @@ export default function Success() {
       </Heading>
       <Text fontSize="lg" color="gray.600">
         Thank you for your purchase!
+      </Text>
+      <Text fontSize="lg" color="gray.600">
+        You will now be redirected to the home page.
       </Text>
     </Box>
   );
