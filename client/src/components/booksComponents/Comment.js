237bd14d2126f8e2b0ff34e6c18a29b749e80a65
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2023-11-15 14:21:21 +1100

    Changed the avatar for the readers comments in booksComponents to moth7 image.

--- client/src/components/booksComponents/Comment.js
+++ client/src/components/booksComponents/Comment.js
@@ -19,8 +19,7 @@ export default function Comment({
   createdAt,
   replies,
 }) {
-  const avatar =
-    "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80";
+  const avatar = "./assets/images/moth7.png";
 
   // State to display the AddReply component
   const [showAddReply, setshowAddReply] = useState(false);
