3c81e1fecff88af6d092e9959283e723350ed4cc
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2023-11-17 17:07:28 +1100

    auth.js on server side: added two more functions in jwt authentification utils: generateResetToken and verifyResetToken for the password reset.

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const secret = "mysecretssshhhhhhh";
 const expiration = "24h";
+const expirationReset = "1h";
 
 module.exports = {
   authMiddleware: function ({ req }) {
@@ -27,5 +28,22 @@ module.exports = {
   signToken: function ({ email, username, _id }) {
     const payload = { email, username, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+
+  // generate a password reset token
+  generateResetToken: function ({ email, username, _id }) {
+    const payload = { email, username, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expirationReset });
+  },
+
+  // verify the reset token
+  verifyResetToken: function (token) {
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expirationReset });
+      return data; // return the decoded data (email, username, _id)
+    } catch (err) {
+      console.log("Invalid token");
+      return err.message("Invalid token");
+    }
   },
 };
