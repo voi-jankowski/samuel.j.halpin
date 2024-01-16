7c668372424ce70c8a591a9cd4a4780d39520d29
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2024-01-17 10:01:53 +1100

    Created an array of objects with all the privacy policy points in PrivacyPolicy.js.

--- client/src/pages/PrivacyPolicy.js
+++ client/src/pages/PrivacyPolicy.js
@@ -1,6 +1,49 @@
 import React from "react";
 import { Container, Heading, Stack, Text } from "@chakra-ui/react";
 
+const privacyPolicySections = [
+  {
+    heading: "Personal Information We Collect",
+    content:
+      "When you register on our site, we may collect your username and email address. This information is essential for you to engage in our website's community, particularly for book discussions and comments.",
+  },
+  {
+    heading: "Use of Personal Information",
+    content:
+      "We use your personal information to identify you on our platform, enabling you to engage in discussions and interact with our content. We commit to not using this information for marketing or advertising purposes, nor do we share it with third parties for such purposes.",
+  },
+  {
+    heading: "Children's Privacy",
+    content:
+      "The Samuel J. Halpin Author Page is not directed at children under the age of 13. We do not knowingly collect personal information from children under this age. If we learn that a child under 13 has provided us with personal information, we will promptly delete it. If you are a parent or guardian and believe your child has provided us with their personal information, please contact us for prompt action.",
+  },
+  {
+    heading: "Respectful Community Engagement",
+    content:
+      "Our community thrives on respect and positivity. We do not tolerate offensive or violent comments and will take immediate action to remove such content. Consistent violation of these standards may result in account termination.",
+  },
+  {
+    heading: "External Links",
+    content:
+      "Our website may contain external links. These linked sites have their own privacy policies, and we bear no responsibility for their content or privacy practices.",
+  },
+  {
+    heading: "Cookies and Tracking",
+    content:
+      "At present, we do not use cookies, except for necessary ones related to Stripe services (currently inactive). We will keep you informed of any changes to our use of cookies.",
+  },
+  {
+    heading: "Policy Updates",
+    content:
+      "We may revise this Privacy Policy periodically. Any changes will be reflected on this page. Please review it regularly for updates.",
+  },
+  {
+    heading: "Contact Information",
+    content:
+      "For any inquiries or concerns about our Privacy Policy, please contact us at [contact email].",
+  },
+];
+
 export default function PrivacyPolicy() {
   return (
     <Container maxW={"4xl"} py="30px">
