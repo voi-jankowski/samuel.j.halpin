08feb83ee9c73cfb020fce988348cc361fef20b1
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2024-01-31 15:45:21 +1100

    Changed the transporter variables to a new email set up for reset in addQuestion in resolvers.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -391,14 +391,12 @@ const resolvers = {
 
         // Send email to the author about the new question
         let transporter = nodemailer.createTransport({
-          service: "gmail",
+          host: process.env.EMAIL_HOST,
+          port: 465,
+          secure: true, // use SSL
           auth: {
-            type: "OAuth2",
             user: process.env.MAIL_USERNAME,
             pass: process.env.MAIL_PASSWORD,
-            clientId: process.env.OAUTH_CLIENTID,
-            clientSecret: process.env.OAUTH_CLIENT_SECRET,
-            refreshToken: process.env.OAUTH_REFRESH_TOKEN,
           },
         });
 
